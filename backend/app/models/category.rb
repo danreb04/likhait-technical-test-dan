2faf6cf0292486ae5af8891bfb7ac59cd9437663
danreb <134129506+danreb04@users.noreply.github.com>
2026-03-13 20:31:54 +0800

    Add validations for Category model attributes

--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   has_many :expenses, dependent: :destroy
+
+  validates :name,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 100 }
 end
